f2179f13aaac9caab81e9c42688f0dae73c2ee5e
zohaib <muhammadzohaibmalik10@gmail.com>
2025-03-22 12:52:10 +0500

    Set up Express server with user routes and database connection; integrate logger for server status

--- user-service/src/index.ts
+++ user-service/src/index.ts
@@ -1,10 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import logger from "./utils/logger.js";
+import connectDB from "./config/db.js";
 
 const app = express();
 dotenv.config();
-
 const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use("/api/v1", userRouter);
+
 app.listen(5000, () => {
-  console.log(`server is running at ${port}`);
+  logger.info(`server is running at ${port}`);
+  connectDB();
 });
