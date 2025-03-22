07555cdf35b19bf6602ff279d0434035812332b2
zohaib <muhammadzohaibmalik10@gmail.com>
2025-03-22 11:16:01 +0500

    add one rule

--- user-service/eslint.config.mjs
+++ user-service/eslint.config.mjs
@@ -3,10 +3,15 @@ import globals from "globals";
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts}"] },
   { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.browser } },
   { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
-  tseslint.configs.recommended,
+  {
+    ...tseslint.configs.recommended, 
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off", 
+      
+    }
+  }
 ]);
