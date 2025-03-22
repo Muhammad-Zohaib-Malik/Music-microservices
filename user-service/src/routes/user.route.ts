dbf5e6abc47eb6d5d89570cec1c3249010f7ece6
zohaib <muhammadzohaibmalik10@gmail.com>
2025-03-22 12:52:01 +0500

    Add user routes for registration and login functionality

---
+++ user-service/src/routes/user.route.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { loginUser, registerUser } from "../controllers/user.controller.js";
+
+const router = Router();
+
+router.post("/user/register", registerUser);
+router.post("/user/login", loginUser);
+
+export default router;
