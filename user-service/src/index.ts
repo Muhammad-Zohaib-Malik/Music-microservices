346e84ca45bf45aaacbc0174c501ffd97ecae9da
zohaib <muhammadzohaibmalik10@gmail.com>
2025-03-23 06:04:35 +0500

    add cookie-parser

--- user-service/src/index.ts
+++ user-service/src/index.ts
@@ -3,12 +3,14 @@ import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import logger from "./utils/logger.js";
 import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 dotenv.config();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/v1", userRouter);
 
 app.listen(5000, () => {
