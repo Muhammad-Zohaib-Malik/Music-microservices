2537b9f89cb1cd73c75d41ced8f3971e9c3cd02e
zohaib <muhammadzohaibmalik10@gmail.com>
2025-03-23 14:12:05 +0500

    add cookie-parser

--- admin-service/src/index.ts
+++ admin-service/src/index.ts
@@ -3,14 +3,24 @@ import dotenv from "dotenv";
 import logger from "./utils/logger.js";
 import { initDB } from "./models/index.js";
 import albumRoutes from "./routes/album.route.js";
+import { v2 as cloudinary } from 'cloudinary';
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 7000;
 
+// Cloudinary configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET_KEY
+});
+
 // Middleware
 app.use(express.json());
+app.use(cookieParser())
 
 // Routes
 app.get("/", (req, res) => {
