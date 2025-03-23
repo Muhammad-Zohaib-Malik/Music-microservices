2c7ffef948d022057abe4e02c26e3f7ee4dab20c
zohaib <muhammadzohaibmalik10@gmail.com>
2025-03-23 06:05:07 +0500

    add myprofile route

--- user-service/src/routes/user.route.ts
+++ user-service/src/routes/user.route.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
-import { loginUser, registerUser } from "../controllers/user.controller.js";
+import {
+  loginUser,
+  myProfile,
+  registerUser,
+} from "../controllers/user.controller.js";
+import { isAuth } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
 router.post("/user/register", registerUser);
 router.post("/user/login", loginUser);
+router.get("/user/me", isAuth, myProfile);
 
 export default router;
