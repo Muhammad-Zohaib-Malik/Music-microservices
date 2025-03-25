26d717e6b0bc2936ac1123944208795c77d9ea3c
zohaib <muhammadzohaibmalik10@gmail.com>
2025-03-25 21:18:45 +0500

    Refactor index.ts for improved code consistency and formatting

--- admin-service/src/index.ts
+++ admin-service/src/index.ts
@@ -3,7 +3,8 @@ import dotenv from "dotenv";
 import logger from "./utils/logger.js";
 import { initDB } from "./models/index.js";
 import albumRoutes from "./routes/album.route.js";
-import { v2 as cloudinary } from 'cloudinary';
+
+import { v2 as cloudinary } from "cloudinary";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
@@ -15,19 +16,19 @@ const port = process.env.PORT || 7000;
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_SECRET_KEY
+  api_secret: process.env.CLOUDINARY_SECRET_KEY,
 });
 
 // Middleware
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
 // Routes
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.use("/api/v1", albumRoutes);
+app.use("/api/v1/", albumRoutes);
 
 initDB().then(() => {
   app.listen(port, () => {
