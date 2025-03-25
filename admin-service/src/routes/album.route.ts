32dcaf310305a79dd2ac6444771e917efca3c21d
zohaib <muhammadzohaibmalik10@gmail.com>
2025-03-25 21:32:50 +0500

    Add album and song routes with admin authentication and file upload support

--- admin-service/src/routes/album.route.ts
+++ admin-service/src/routes/album.route.ts
@@ -1,5 +1,20 @@
 import { Router } from "express";
+import { isAdminAuth } from "../middleware/auth.middleware.js";
+import {
+  AddAlbum,
+  AddSong,
+  AddThumbnail,
+  DeleteAlbum,
+  DeleteSong,
+} from "../controllers/album.controller.js";
+import { upload } from "../middleware/multer.js";
 
 const router = Router();
+
+router.post("/album/new", isAdminAuth, upload.single("file"), AddAlbum);
+router.post("/song/new", isAdminAuth, upload.single("file"), AddSong);
+router.post("/song/:id", isAdminAuth, upload.single("file"), AddThumbnail);
+router.delete("/album/:id", isAdminAuth, DeleteAlbum);
+router.delete("/song/:id", isAdminAuth, DeleteSong);
 
 export default router;
