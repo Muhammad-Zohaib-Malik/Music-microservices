143863d064277225ac86a432a3c1c8fad2f6f1f8
zohaib <muhammadzohaibmalik10@gmail.com>
2025-03-25 21:19:09 +0500

    Refactor multer configuration to simplify storage setup

--- admin-service/src/middleware/multer.ts
+++ admin-service/src/middleware/multer.ts
@@ -1,13 +1,6 @@
-import multer from 'multer';
+import multer from "multer";
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads/'); 
-  },
-  filename: (req, file, cb) => {
-    cb(null, `${Date.now()}-${file.originalname}`); 
-  },
-});
+const storage = multer.diskStorage({});
 export const upload = multer({
   storage,
 });
